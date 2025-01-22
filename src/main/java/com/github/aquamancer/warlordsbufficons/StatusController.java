6233ebb69c9a4b557b25490fdbf831c4e48ffd75
aquamancer <endlessshat@gmail.com>
2025-01-22 12:42:33 -0500

    dev: add statuses.json

--- src/main/java/com/github/aquamancer/warlordsbufficons/StatusController.java
+++ src/main/java/com/github/aquamancer/warlordsbufficons/StatusController.java
@@ -2,15 +2,20 @@ package com.github.aquamancer.warlordsbufficons;
 
 import com.github.aquamancer.warlordsbufficons.statuses.BuffEnum;
 import com.github.aquamancer.warlordsbufficons.statuses.DebuffEnum;
+import net.minecraft.util.IChatComponent;
 
 import java.util.List;
 
 public class StatusController {
-    private List<Status> statuses;
+    private static List<Status> statuses;
+
     public static void onChatBuff(BuffEnum buff) {
-        
+
     }
     public static void onChatDebuff(DebuffEnum debuff) {
         
+    }
+    public static void onActionBarChanged(IChatComponent bar) {
+
     }
 }
--- src/main/java/com/github/aquamancer/warlordsbufficons/handlers/ActionBarHandler.java
+++ src/main/java/com/github/aquamancer/warlordsbufficons/handlers/ActionBarHandler.java
@@ -1,13 +1,13 @@
 package com.github.aquamancer.warlordsbufficons.handlers;
 
+import com.github.aquamancer.warlordsbufficons.StatusController;
 import net.minecraftforge.client.event.ClientChatReceivedEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ActionBarHandler {
     @SubscribeEvent
-    public void onActionBarChanged(ClientChatReceivedEvent event) {
-        if (event.type == 2) {
-            // handle event
-        }
+    public void onChatMessageReceived(ClientChatReceivedEvent event) {
+        if (event.type != 2) return;
+        StatusController.onActionBarChanged(event.message);
     }
 }
